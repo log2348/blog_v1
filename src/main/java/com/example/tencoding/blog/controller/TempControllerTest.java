fb3dd0e6d61124ea308727bf47adbb7810f5ef66
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-06-16 10:14:09 +0900

    yml 파일 추가 설정
    jpa 설정 추가, jsp 설정 추가

---
+++ src/main/java/com/example/tencoding/blog/controller/TempControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.tencoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TempControllerTest {
+
+	// http://localhost:8081/blog/temp/home
+	@GetMapping("/temp/home")
+	public String tempHome() {
+		// 파일 리턴 경로 : src/main/resources/static
+		// 리턴명 : /home.html
+		//
+		System.out.println("tempHome()");
+		return "/home.html";
+	}
+}
