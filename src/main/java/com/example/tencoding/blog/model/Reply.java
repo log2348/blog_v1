8f29c8b1f48fd489cc96713fece2aed184bcd095
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-06-16 11:28:46 +0900

    ORM 테이블 생성

---
+++ src/main/java/com/example/tencoding/blog/model/Reply.java
@@ -0,0 +1,44 @@
+package com.example.tencoding.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Reply {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false, length = 200)
+	private String content;
+
+	@ManyToOne // 여러 개의 댓글은 하나의 게시글에 존재할 수 있다
+	@JoinColumn(name = "boardId")
+	private Board board;
+
+	// 하나의 댓글에 여러 유저가 작성을 할 수 있는가?
+	@ManyToOne
+	@JoinColumn(name = "userId") // <---- Reply 테이블의 컬럼명이 된다
+	private User user;
+
+	@CreationTimestamp
+	private Timestamp createDate;
+
+}
---
+++ src/main/java/com/example/tencoding/blog/model/User.java
@@ -0,0 +1,41 @@
+package com.example.tencoding.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity // User 클래스가 자동으로 MySQL에 테이블을 생성한다
+public class User {
+
+	@Id // Primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에 연결된 DB의 넘버링 전략을 따라가겠다
+	private int id;
+	@Column(nullable = false, length = 30)
+	private String username;
+	@Column(nullable = false, length = 100)
+	private String password;
+	@Column(nullable = false, length = 50)
+	private String email;
+	@CreationTimestamp // 시간이 자동으로 입력된다
+	private Timestamp createDate;
+
+	// Domain - 데이터의 범주화 (User, user 등 잘못입력)
+	@ColumnDefault("'user'")
+	private String role; // Enum 타입 사용 권장 : admin, user, manager
+}
