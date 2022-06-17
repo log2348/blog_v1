8bddc67e391129a5f3c704d5ebedfcb3cd1eec2c
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-06-17 15:43:28 +0900

    회원가입 기초 세팅

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,19 @@
+let index = {
+	
+	init: function() {
+		$("#btn-save").bind("click", () => {
+			this.save();
+		});
+	},
+	
+	save: function()  {
+		let data = {
+			username:$("#username").val(),
+			password:$("#password").val(),
+			email: $("#email").val()
+		}
+	console.log(data);
+	}	
+}
+
+index.init();
